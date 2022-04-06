9164064e8b9275c7e92f464d88b9ed3e9f52c424
Peter Sims <pas2182@columbia.edu>
2022-04-05 21:55:25 -0400

    more stringent unique fragment collapse

--- count_dna.py
+++ count_dna.py
@@ -1,27 +1,58 @@
 #! /usr/bin/python
 import gzip
 import io
+import multiprocessing as mp
+from math import ceil
+from os import getpid
 
-def addressct(address,addressct,stats):
+def collapse(addresses,cbcchs):
+    keepdict={}
+    for cbcch in cbcchs:
+        reads = addresses[cbcch]
+        keep = [1 for read in reads]
+        for i,read1 in enumerate(reads):
+            if keep[i]==1:
+                p1=read1[1]
+                p2=read1[2]
+                for j,read2 in enumerate(reads):
+                    if read2[1]==p1 or read2[2]==p2:
+                        keep[j]=0
+                        break
+        keepdict[cbcch]=keep
+    return keepdict
+
+
+def addressct(address,addressct,stats,threads):
+    r1addresses = set()
+    r2addresses = set()
     addresses = {}
+    readcts={}
     with io.BufferedReader(gzip.open(address,'rb')) as f:
         for line in f:
             dlist = line.decode().split()
-            cbc = dlist[1]
-            tup = dlist[2],dlist[3],dlist[4],dlist[7]
-            if cbc in addresses:
-                addresses[cbc].append(tup)
+            cbc=dlist[1]
+            ch=dlist[2]
+            p1add = cbc,ch,dlist[3],dlist[7]
+            p2add = cbc,ch,dlist[4],dlist[7]
+            if cbc in readcts:
+                readcts[cbc]+=1
             else:
-                addresses[cbc] = [tup]
-    readcts = {cbc:len(addresses[cbc]) for cbc in addresses}
-    addresses = {cbc:set(addresses[cbc]) for cbc in addresses}
+                readcts[cbc]=1
+            if p1add not in r1addresses and p2add not in r2addresses:
+                r1addresses.add(p1add)
+                r2addresses.add(p2add)
+                if cbc in addresses:
+                    addresses[cbc].append((ch,dlist[3],dlist[4],dlist[7]))
+                else:
+                    addresses[cbc] = [(ch,dlist[3],dlist[4],dlist[7])]  
     with open(addressct,'w') as g1, open(stats,'w') as g2:
         for cbc in addresses:
-            for tup in addresses[cbc]:
+            address=addresses[cbc]
+            for tup in address:
                 st=cbc+'\t'+'\t'.join([p for p in tup])+'\n'
                 g1.write(st)
             reads = readcts[cbc]
-            frags = len(addresses[cbc])
+            frags = len(address)
             st = cbc+'\t'+str(reads)+'\t'+str(frags)+'\n'
             g2.write(st)
     return 0
