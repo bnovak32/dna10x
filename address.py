1bdc331c5b3cc5ed0d611a2ceb62d533ff10b8bf
Ubuntu <ubuntu@ip-172-31-31-164.ec2.internal>
2022-10-09 19:34:42 +0000

    add cutadapt feature

--- address.py
+++ address.py
@@ -3,78 +3,52 @@ from pysam import AlignmentFile
 import gzip
 import io
 
-def address(address,bamout,bclen,bcset,alignment_score,insert_size):
+
+
+def address(address,bamout,bclen,bcset,insert_size):
     cbcs = []
     qcbcs = []
     cbcfreq_dict = {}   
+    rdict={}
     tot=0 
     with gzip.open(address, 'wb') as g:
         with AlignmentFile(bamout,'rb') as f:
             for read in f:
-                score = int(read.get_tag('AS'))
-                isize = int(read.tlen)
-                if read.is_read1:
-                    if score>alignment_score and abs(isize)<insert_size and isize!=0:
-                        readid = ':'.join(read.qname.split(':')[3:7])
-                        cbc = read.get_tag('BC')[0:16]
-                        ch = f.getrname(read.reference_id)
-                        cbcs.append(cbc)
-                        if cbc in bcset:
-                            if cbc in cbcfreq_dict:
-                                cbcfreq_dict[cbc]+=1
-                            else:
-                                cbcfreq_dict[cbc]=1
-                            qcbcs.append('0')
-                            tot+=1
-                        else:
-                            qcbcs.append(read.get_tag('BC')[16::])
-                        if isize>0:
-                            p1 = read.reference_start+4
-                            p2 = p1+isize-5
-                        else:
-                            p1 = read.next_reference_start+4
-                            p2 = p1-isize-5
-                        newline=readid+'\t'+cbc+'\t'+ch+'\t'+str(p1)+'\t'+str(p2)+'\t'+str(score)+'\t'+str(isize)+'\n'
-                        g.write(newline.encode())   
-    for cbc in cbcfreq_dict:
-        cbcfreq_dict[cbc]=cbcfreq_dict[cbc]/tot
-    return cbcs,qcbcs,cbcfreq_dict
-
-def contig_address(address,chrs,bamout,bclen,bcset,alignment_score,insert_size):
-    outfiles = [address+'.'+ch+'.address.txt.gz' for ch in chrs]
-    outputs = {ch:gzip.open(outfile,'wb') for ch,outfile in zip(chrs,outfiles)}
-    ch_cbcdict={ch:[] for ch in chrs}
-    ch_qcbcdict={ch:[] for ch in chrs}
-    cbcfreq_dict = {}
-    tot=0
-    with AlignmentFile(bamout,'rb') as f:
-        for read in f:
-            if read.is_read1:
-                score = int(read.get_tag('AS'))
-                isize = int(read.tlen)
-                if score>alignment_score and abs(isize)<insert_size and isize!=0:
-                    readid = ':'.join(read.qname.split(':')[3:7])
-                    cbc = read.get_tag('BC')[0:bclen]
+                score = read.get_tag('AS')
+                isize = read.tlen
+                rlen= read.rlen
+                if read.is_read1 and score*isize!=0 and abs(isize)<insert_size:
+                    readid = ':'.join(read.qname.split(':')[2:7])
+                    cbc = read.get_tag('BC')[0:16]
                     ch = f.getrname(read.reference_id)
+                    cbcs.append(cbc)
+                    if cbc in bcset:
+                        if cbc in cbcfreq_dict:
+                            cbcfreq_dict[cbc]+=1
+                        else:
+                            cbcfreq_dict[cbc]=1
+                        qcbcs.append('0')
+                        tot+=1
+                    else:
+                        qcbcs.append(read.get_tag('BC')[16::])
                     if isize>0:
                         p1 = read.reference_start+4
                         p2 = p1+isize-5
                     else:
                         p1 = read.next_reference_start+4
                         p2 = p1-isize-5
-                    newline=readid+'\t'+cbc+'\t'+ch+'\t'+str(p1)+'\t'+str(p2)+'\t'+str(score)+'\t'+str(isize)+'\n'
-                    outputs[ch].write(newline.encode())
-                    ch_cbcdict[ch].append(cbc)
-                    if cbc in bcset:
-                        if cbc in cbcfreq_dict:
-                            cbcfreq_dict[cbc]+=1
-                        else:
-                            cbcfreq_dict[cbc]=1
-                        ch_qcbcdict[ch].append('0')
-                        tot+=1
-                    else:
-                        ch_qcbcdict[ch].append(read.get_tag('BC')[bclen::])
+                    newline=readid+'\t'+cbc+'\t'+ch+'\t'+str(p1)+'\t'+str(p2)+'\t'+str(rlen)+'\t'+str(score)+'\t'+str(isize)
+                    rdict[readid]=newline
+                else:
+                    readid = ':'.join(read.qname.split(':')[2:7])
+                    if readid in rdict:
+                        score = read.get_tag('AS')
+                        if score>0:
+                            rlen=read.rlen
+                            newline = rdict[readid]+'\t'+str(rlen)+'\t'+str(score)+'\n'
+                            g.write(newline.encode())
+                            del rdict[readid]
     for cbc in cbcfreq_dict:
         cbcfreq_dict[cbc]=cbcfreq_dict[cbc]/tot
-    return ch_cbcdict,ch_qcbcdict,cbcfreq_dict
+    return cbcs,qcbcs,cbcfreq_dict
 
--- dna10x.py
+++ dna10x.py
@@ -8,7 +8,7 @@ from glob import glob
 from pysam import AlignmentFile
 from count_dna import chrfragments,chrfragments_output,fragments
 import subprocess
-from address import address,contig_address
+from address import address
 from error_correct import cbccorrect,revcomp
 from functools import partial
 from multiprocessing import Pool
@@ -21,13 +21,13 @@ def parse_user_input():
     parser.add_argument('-b','--barcodes',required=True,help='Path to 10x barcode whitelist.')
     parser.add_argument('-t','--threads',required=True,type=int,help='Number of cpu threads.')
     parser.add_argument('-r','--reference',required=True,help='Path to reference genome.')
-    parser.add_argument('-a','--alignment-score',type=int,required=True,help='Minimum alignment score.')
     parser.add_argument('-i','--insert-size',required=True,type=int,help='Maximum insert size.')
     parser.add_argument('-p','--barcode-position',type=int,required=True,help='1-indexed cell barcode position in read 2.')
     parser.add_argument('-rc','--revcomp',help='Reverse complement cell barcodes.',action='store_true')
-    parser.add_argument('-pc','--parallel-contig',action='store_true',help='Run fragment counting for contigs in parallel.')
     parser.add_argument('-sf','--skip-fastq',action='store_true',help='Skip fastq generation if they already exist.')
     parser.add_argument('-sa','--skip-align',action='store_true',help='Skip alignment with bwa mem if bam already exists.')
+    parser.add_argument('-c','--cutadapt',action='store_true',help='Run cutadapt on interleaved fastq.')
+    parser.add_argument('-ad','--adapter',required=False,help='Adapter sequence to be trimmed by cutadapt.')
     return parser
 
 parser = parse_user_input()
@@ -74,16 +74,23 @@ for sample in samples:
             fastqout = directory+'/outs/fastq_path/'+project+'/'+sample+'/'+sample+'_'+str(j)+'.fastq.gz'
             cmd='python call_demux.py -r1 %(r1)s -r2 %(r2)s -r3 %(r3)s -p %(pos)d| gzip > %(fastqout)s' % vars()
             print(cmd)
-            p=subprocess.Popen(cmd,shell=True)
-            procs.append(p)
+#            p=subprocess.Popen(cmd,shell=True)
+#            procs.append(p)
             fastqouts.append(fastqout)
             j+=1
-        p_exit = [p.wait() for p in procs]
+#p_exit = [p.wait() for p in procs]
 
         fqs = ' '.join(fastqouts)
-        cmd = "bwa mem -p -C -M -t %(threads)d %(reference)s '<zcat %(fqs)s' | samtools view -Sb - > %(bamout)s" % vars()
-        print(cmd)
-        os.system(cmd)
+        print(fastqouts)
+        if not ui.cutadapt:
+            cmd = "bwa mem -p -C -M -t %(threads)d %(reference)s '<zcat %(fqs)s' | samtools view -Sb - > %(bamout)s" % vars()
+            print(cmd)
+            os.system(cmd)
+        else:
+            adapter = ui.adapter
+            cmd = "zcat %(fqs)s | cutadapt -a %(adapter)s -A %(adapter)s --interleaved --cores=%(threads)s -o - - | bwa mem -p -C -M -t %(threads)s %(reference)s - | samtools view -Sb - > %(bamout)s" % vars()
+            print(cmd)
+            os.system(cmd)
 
     addressfile=directory+'/outs/fastq_path/'+project+'/'+sample+'/'+sample+'.address.txt.gz'    
     fragfile=directory+'/outs/fastq_path/'+project+'/'+sample+'/'+sample+'.fragments.tsv'
@@ -99,20 +106,7 @@ for sample in samples:
         print("Error: can't find barcode whitelist file.")
         exit()   
      
-    if ui.parallel_contig:
-        if __name__ == '__main__':
-            addressfile=directory+'/outs/fastq_path/'+project+'/'+sample+'/'+sample
-            ch_cbcdict,ch_qcbcdict,cbcfreq_dict=contig_address(addressfile,chrs,bamout,bclen,bcset,ui.alignment_score,ui.insert_size)
-            partial_correct=partial(cbccorrect, cbcfreq_dict=cbcfreq_dict)
-            with Pool(processes=len(chrs)) as pool:
-                newcbcs=pool.starmap(partial_correct,
-                    iterable=zip([ch_cbcdict[ch] for ch in chrs],[ch_qcbcdict[ch] for ch in chrs]))
-            with Pool(processes=len(chrs)) as pool:
-                fragdicts = pool.starmap(chrfragments,
-                    iterable=zip([addressfile+'.'+ch+'.address.txt.gz' for ch in chrs],newcbcs))
-            chrfragments_output(sample,fragfile,reference,fragdicts,chrs)
-    else:
-        cbcs,qcbcs,cbcfreq_dict=address(addressfile,bamout,bclen,bcset,ui.alignment_score,ui.insert_size)
-        newcbcs=cbccorrect(cbcs,qcbcs,cbcfreq_dict) 
-        fragments(sample,reference,addressfile,fragfile,chrs,newcbcs)    
+    cbcs,qcbcs,cbcfreq_dict=address(addressfile,bamout,bclen,bcset,ui.insert_size)
+    newcbcs=cbccorrect(cbcs,qcbcs,cbcfreq_dict) 
+    fragments(sample,reference,addressfile,fragfile,chrs,newcbcs)    
 
